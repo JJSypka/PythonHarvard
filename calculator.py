c14ff72705ce5a261c72298c9bf71f9bf5bbcce0
Jędrzej Sypka <jj@macbook-air-jedrzej.home>
2023-11-21 22:15:43 +0100

    4

--- calculator.py
+++ calculator.py
@@ -1,19 +1,22 @@
+# obiektowo
 def main():
     x = int (input("Whats x?"))
     print("x squared is", square(x))
     
 def square(n):
-    return pow(n,2)
+    return n * n
 
-main()
+if __name__ == "__main__":
+    main()
+# zwykle
+# x = int(input("Whats x?"))
+#y = int(input("Whats y?"))
+#z = round(x +y)
 
-x = int(input("Whats x?"))
-y = int(input("Whats y?"))
-z = round(x +y)
-print(x +y)
-print(f"{z:,}")
+#print(x +y)
+#print(f"{z:,}")
 #You can also write
-print(int(input("Whats x?")) + int(input("Whats y?")))
+#print(int(input("Whats x?")) + int(input("Whats y?")))
 
-z = (x/y)
-print (f"{z:.2f}")
+#z = (x/y)
+#print (f"{z:.2f}")
